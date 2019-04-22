e6ad4886c34952545efcc91351b62dcae98686a0
Sjors Robroek <s.robroek@vxsan.com>
2019-04-22 16:34:10 +0400

    changed reporter

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-inspec exec ./inspec-controls -t vsphere://vcsa-01.lab.vxsan.com --user 'administrator@lab.vxsan.com' --password 'VMware1!VMware1!'  --attrs ./inspec-controls/params.yml --insecure
+inspec exec ./inspec-controls -t vsphere://vcsa-01.lab.vxsan.com --user 'administrator@lab.vxsan.com' --password 'VMware1!VMware1!'  --attrs ./inspec-controls/params.yml --insecure --reporter documentation
