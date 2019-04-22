7a097925d61a069f3751bc7570a5a3d6e1f5d67c
Sjors Robroek <s.robroek@vxsan.com>
2019-04-22 16:11:24 +0400

    fixed script

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-inspec exec ./inspec-profiles -t vsphere://vcsa-01.lab.vxsan.com --user 'administrator@lab.vxsan.com' --password 'VMware1!VMware1!'  --attrs ./inspec-profiles/params.yml --insecure
+inspec exec ./inspec-controls -t vsphere://vcsa-01.lab.vxsan.com --user 'administrator@lab.vxsan.com' --password 'VMware1!VMware1!'  --attrs ./inspec-controls/params.yml --insecure
